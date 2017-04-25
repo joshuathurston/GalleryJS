9b2df43b048275ffc800b9ea0d37628ee1eb4beb
joshuathurston <jthurston3@mail.csuchico.edu>
2017-04-25 11:22:59 -0700

    x24 finished

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
-
+var chico = ["Mall", "Bidwell Park", "Chico state", "Bidwell Mansion", "Gateway Science Museum"];
+var current;
 
 function shuffleGallery()
 {
-
+  var randomIndex;
+  do
+  {
+    randomIndex = Math.floor(Math.random() * chico.length);
+  }while(randomIndex == current);
+  current = randomIndex;
+  console.log(chico[current]);
 }

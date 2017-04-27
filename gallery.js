819d82aa57f1ac36a04943c60e43102e67b4b115
joshuathurston <jthurston3@mail.csuchico.edu>
2017-04-27 11:56:31 -0700

    x24 part 2 updated

--- gallery.js
+++ gallery.js
@@ -1,5 +1,23 @@
-var chico = ["Mall", "Bidwell Park", "Chico state", "Bidwell Mansion", "Gateway Science Museum"];
+//var chico = ["Mall", "Bidwell Park", "Chico state", "Bidwell Mansion", "Gateway Science Museum"];
 var current;
+var chico = [
+  {description: "Mall",
+   picture: "http://www.centennialrec.com/images/made/a7a5d8042b20c99d/Chico_Mall_234_582_388_c1.jpg",
+   from: "centennialrec.com"
+  },
+  {description: "Bidwell Park",
+   picture: "http://www.csuchico.edu/our-region/about-town/images/bidwell1.jpg",
+   from: "csuchico.edu"
+  },
+  {description: "Bidwell Mansion",
+   picture: "http://www.csuchico.edu/our-region/about-town/images/bidwell.jpg",
+   from: "csuchico.edu"
+  },
+  {description: "Gateway Science Museum",
+   picture: "http://www.csuchico.edu/gateway/_assets/images/rotating-interior/augers.jpg",
+   from: "csuchico.edu"
+  }
+];
 
 function shuffleGallery()
 {
